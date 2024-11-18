2286abc49683baa463ba8ab1453bdffe7e1d0e19
Tnalxmsk <alstnwkd990@naver.com>
2024-11-19 00:15:33 +0900

    :sparkles: feat: 자립 목표 Tab 구성 요소 구현
    현재 목표 및 Progressbar
    자립목표 리스트 컴포넌트 구현

---
+++ src/components/common/GoalList.tsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import washer from '../../assets/images/washer.png';
+import door from '../../assets/images/door.png';
+import money from '../../assets/images/money.png';
+import clean from '../../assets/images/clean.png';
+import DoneCard from './DoneCard.tsx';
+
+const goalList = [
+  {
+    id: 1,
+    goalType: '일상생활기술',
+    image: washer,
+    progressType: '진행중',
+  },
+  {
+    id: 2,
+    goalType: '자기관리기술',
+    image: clean,
+    progressType: '완료',
+  },
+  {
+    id: 3,
+    goalType: '돈관리기술',
+    image: money,
+    progressType: '진행전',
+  },
+  {
+    id: 4,
+    goalType: '사회진출기술',
+    image: door,
+    progressType: '진행전',
+  },
+];
+
+interface GoalListProps {
+  enabled: boolean;
+}
+
+const GoalList = ({ enabled }: GoalListProps) => {
+
+  return (
+    <GoalListContainer>
+      {goalList.map((goal) => (
+        <GoalContainer key={goal.id} onClick={enabled ? () => /*todo*/undefined : undefined}>
+          {goal.progressType !== '진행전' ? (
+            <DoneCardWrapper>
+              <DoneCard label={goal.progressType} />
+            </DoneCardWrapper>
+          ) : null}
+          <Image src={goal.image} alt={goal.goalType} />
+          <GoalTypeText>{goal.goalType}</GoalTypeText>
+        </GoalContainer>
+      ))}
+    </GoalListContainer>
+  );
+};
+
+export default GoalList;
+
+const GoalListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 12px;
+
+  & > div {
+    flex: 0 1 calc(50% - 12px);
+    max-width: 210px;
+    box-sizing: border-box;
+  }
+`;
+
+const DoneCardWrapper = styled.div`
+  position: absolute;
+  top: 12px;
+  left: 12px;
+`;
+
+const Image = styled.img`
+  width: 130px;
+  height: 130px;
+  margin-top: 20px;
+`;
+
+const GoalContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 210px;
+  background-color: white;
+  border-radius: 30px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+`;
+
+const GoalTypeText = styled.div`
+  margin-top: 6px;
+  font: ${({ theme }) => theme.fonts.body_sb_18px};
+  color: ${({ theme }) => theme.colors.gray900};
+`;
