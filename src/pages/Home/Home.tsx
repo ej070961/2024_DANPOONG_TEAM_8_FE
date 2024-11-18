0505420a79383031c517bcab63185ce29723f29b
ej070961 <ej070961@gmail.com>
2024-11-19 00:05:27 +0900

    ✨ Feat: 공통 내비게이션 컴포넌트 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
-
+import styled from 'styled-components';
+import BackgroundImg from '../../assets/images/home_bg.png';
+import NavBar from '../../components/Common/NavBar';
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Wrapper>
+      <NavBar />
+    </Wrapper>
+  );
 }
 
 export default Home;
+const Wrapper = styled.div`
+  width: 100vw;
+  max-width: 480px;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  background-image: url(${BackgroundImg});
+  background-size: 100vw 100vh;
+`;
