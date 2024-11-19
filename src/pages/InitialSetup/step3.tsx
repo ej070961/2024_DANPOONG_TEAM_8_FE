bf474b0508a0884a9c6f671fa3429860d74efed4
ej070961 <ej070961@gmail.com>
2024-11-20 02:05:58 +0700

    ✨ Feat: 회원가가입 시 초기  목표설정, 사전점검 화면 구현

--- src/pages/InitialSetup/step3.tsx
+++ src/pages/InitialSetup/step3.tsx
@@ -1,10 +1,16 @@
 //어떤 사람인지 선택하는 화면
 import styled from 'styled-components';
 import Header from '../../components/InitialSetup/Header';
+import CheckGuide from '../../components/goal/CheckGuide';
+import CheckCardList from '../../components/goal/CheckCardList';
 function Step3() {
   return (
     <Wrapper>
       <Header step={3} />
+      <GuideContainer>
+        <CheckGuide />
+      </GuideContainer>
+      <CheckCardList />
     </Wrapper>
   );
 }
@@ -15,8 +21,12 @@ const Wrapper = styled.div`
   max-width: 480px;
   width: 100vw;
   height: 100vh;
-
   display: flex;
   flex-direction: column;
-  align-items: center;
+  background: linear-gradient(90deg, #7676ff 0%, #c5c5ff 67%, #d6ecff 100%);
+`;
+const GuideContainer = styled.div`
+  position: relative;
+  width: 100%;
+  margin: 56px 16px 44px 16px;
 `;
