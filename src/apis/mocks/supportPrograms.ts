e30249639c74a02380518b8fe3278250a5eff8ee
Tnalxmsk <alstnwkd990@naver.com>
2024-11-20 01:34:47 +0900

    :sparkles: feat: 지원 제도 목록 구현

---
+++ src/apis/mocks/supportPrograms.ts
@@ -0,0 +1,40 @@
+import { SupportProgram } from '../../types';
+
+export const supportPrograms: SupportProgram[] = [
+  {
+    id: 0,
+    supportType: '경제',
+    supportTitle: '청년 도약 계좌',
+    supportSubTitle: '만 19세~34세 이하',
+  },
+  {
+    id: 1,
+    supportType: '진로',
+    supportTitle: '국가근로장학금',
+    supportSubTitle: '학자금지원 9구간 이하',
+  },
+  {
+    id: 2,
+    supportType: '주거',
+    supportTitle: '자립준비청년 주거지원',
+    supportSubTitle: '아동복지시설 퇴소 5년 이내',
+  },
+  {
+    id: 3,
+    supportType: '경제',
+    supportTitle: '청년 도약 계좌',
+    supportSubTitle: '만 19세~34세 이하',
+  },
+  {
+    id: 4,
+    supportType: '진로',
+    supportTitle: '국가근로장학금',
+    supportSubTitle: '학자금지원 9구간 이하',
+  },
+  {
+    id: 5,
+    supportType: '주거',
+    supportTitle: '자립준비청년 주거지원',
+    supportSubTitle: '아동복지시설 퇴소 5년 이내',
+  },
+];
