383cb6477c11d9c70e9f065228e46e70a0e49df1
Tnalxmsk <alstnwkd990@naver.com>
2024-11-21 20:42:25 +0900

    :sparkles: feat: 캐릭터 타입 정의
    특정 캐릭터 타입에 따라 src 경로 K V 형태로 정의

---
+++ src/constant/character.ts
@@ -0,0 +1,6 @@
+export const CharacterType: Record<string, string> = {
+  CHICK: '/src/assets/images/chick.png',
+  CAT: 'src/assets/images/cat.png',
+  RABBIT: 'src/assets/images/rabbit.png',
+  BEAR: 'src/assets/images/bear.png',
+};
