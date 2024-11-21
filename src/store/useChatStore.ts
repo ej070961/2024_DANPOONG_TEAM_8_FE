5973748e95b4e13c0e13430062ee8b65ffb19097
Tnalxmsk <alstnwkd990@naver.com>
2024-11-21 15:42:38 +0900

    :sparkles: feat: 채팅 store 구현
    채팅 내역 저장을 위해 구현

---
+++ src/store/useChatStore.ts
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+import { Message } from '../@type/chat.ts';
+
+interface ChatStore {
+  messages: Message[];
+  addMessage: (message: Message) => void;
+}
+
+export const useChatStore = create<ChatStore>((set) => ({
+  messages: [],
+  addMessage: (message) => set((state) => ({ messages: [...state.messages, message] })),
+}));
