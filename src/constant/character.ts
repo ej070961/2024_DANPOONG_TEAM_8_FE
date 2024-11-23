4c5b2fcade0adca6e841c6a603fe2b55ef6700a3
ej070961 <ej070961@gmail.com>
2024-11-24 00:16:03 +0900

    🐛 Fix: 캐릭터 이미지 경로 수정

--- src/constant/character.ts
+++ src/constant/character.ts
@@ -1,13 +1,22 @@
+import ChickPng from '../assets/images/chick.png';
+import CatPng from '../assets/images/cat.png';
+import RabbitPng from '../assets/images/rabbit.png';
+import BearPng from '../assets/images/bear.png';
+
+import Egg from '../assets/images/egg.png';
+import Egg2 from '../assets/images/egg2.png';
+import Slime from '../assets/images/slime.png';
+import Slime2 from '../assets/images/slime2.png';
 export const CharacterLevel: Record<number, string> = {
-  1: '/src/assets/images/egg.png',
-  2: '/src/assets/images/egg2.png',
-  3: '/src/assets/images/slime.png',
-  4: '/src/assets/images/slime2.png',
+  1: Egg,
+  2: Egg2,
+  3: Slime,
+  4: Slime2,
 };
 
 export const CharacterType: Record<string, string> = {
-  CHICK: '/src/assets/images/chick.png',
-  CAT: '/src/assets/images/cat.png',
-  RABBIT: '/src/assets/images/rabbit.png',
-  BEAR: '/src/assets/images/bear.png',
+  CHICK: ChickPng,
+  CAT: CatPng,
+  RABBIT: RabbitPng,
+  BEAR: BearPng,
 };
