80ca10a39201826014bb0ae461371c385f6b8e17
ej070961 <ej070961@gmail.com>
2024-11-23 10:14:02 +0900

    🐛 Fix: 캐릭터 레벨에 따라 렌더링 되도록 수정

--- src/constant/character.ts
+++ src/constant/character.ts
@@ -1,3 +1,10 @@
+export const CharacterLevel: Record<number, string> = {
+  1: '/src/assets/images/egg.png',
+  2: '/src/assets/images/egg2.png',
+  3: '/src/assets/images/slime.png',
+  4: '/src/assets/images/slime2.png',
+};
+
 export const CharacterType: Record<string, string> = {
   CHICK: '/src/assets/images/chick.png',
   CAT: '/src/assets/images/cat.png',
